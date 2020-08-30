d740b19eb86097687f02d4cfd50de56a698d5f0b
BudgetToaster <28935064+BanePig@users.noreply.github.com>
2020-08-29 20:08:56 -0400

    Octree data is now stored seperately from the Model, and in a different format. This format also now supports the easy conversion of triangle meshes.

---
+++ SVO/TriBoxOverlap.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+
+namespace SVO
+{
+    public class TriBoxOverlap
+    {
+        // https://stackoverflow.com/questions/17458562/efficient-aabb-triangle-intersection-in-c-sharp
+        public static bool IsIntersecting(Bounds box, Vector3[] triangle)
+        {
+            float triangleMin, triangleMax;
+            
+            // Test the box normals (x-, y- and z-axes)
+            var boxNormals = new[] {
+                new Vector3(1,0,0),
+                new Vector3(0,1,0),
+                new Vector3(0,0,1)
+            };
+            for (int i = 0; i < 3; i++)
+            {
+                Project(triangle, boxNormals[i], out triangleMin, out triangleMax);
+                if (triangleMax < box.min[i] || triangleMin > box.max[i])
+                    return false; // No intersection possible.
+            }
+
+            // Test the triangle normal
+            var boxVertices = new Vector3[8];
+            for(var i = 0; i < 8; i++)
+                boxVertices[i] = box.min;
+            for (var i = 0; i < 8; i++)
+            {
+                if ((i & 4) > 0) boxVertices[i].x += box.size.x;
+                if ((i & 2) > 0) boxVertices[i].y += box.size.y;
+                if ((i & 1) > 0) boxVertices[i].z += box.size.z;
+            }
+            var triangleNorm = Vector3.Cross(triangle[0] - triangle[1], triangle[2] - triangle[1]);
+            float triangleOffset = Vector3.Dot(triangleNorm, triangle[0]);
+            Project(boxVertices, triangleNorm, out var boxMin, out var boxMax);
+            if (boxMax < triangleOffset || boxMin > triangleOffset)
+                return false; // No intersection possible.
+
+            // Test the nine edge cross-products
+            Vector3[] triangleEdges = {
+                triangle[0] - triangle[1],
+                triangle[1] - triangle[2],
+                triangle[2] - triangle[0]
+            };
+            for (var i = 0; i < 3; i++)
+            {
+                for (var j = 0; j < 3; j++)
+                {
+                    // The box normals are the same as it's edge tangents
+                    var axis = Vector3.Cross(triangleEdges[i], boxNormals[j]);
+                    Project(boxVertices, axis, out boxMin, out boxMax);
+                    Project(triangle, axis, out triangleMin, out triangleMax);
+                    if (boxMax < triangleMin || boxMin > triangleMax)
+                        return false; // No intersection possible
+                }
+            }
+
+            // No separating axis found.
+            return true;
+        }
+
+        private static void Project(Vector3[] points, Vector3 axis, out float min, out float max)
+        {
+            min = float.PositiveInfinity;
+            max = float.NegativeInfinity;
+            foreach (var p in points)
+            {
+                var val = Vector3.Dot(axis, p);
+                if (val < min) min = val;
+                if (val > max) max = val;
+            }
+        }
+    }
+}
